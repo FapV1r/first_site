8a41ce733c3406534f8c0ca05dd1ff62b87b6f20
магомед рамазанович <magomedramazanovich@MacBook-Air-magomed.local>
2026-04-15 12:10:08 +0300

    update_commit

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const sign_in = document.getElementById("Sign_in")
 const sign_up = document.getElementById("Sign_up")
 
 sign_in.addEventListener('click', ()=> {
-    window.location.href= "sign_in.html"
+    window.location.href= "/sing/sign_in.html"
 })
 
 sign_up.addEventListener('click', ()=> {
-    window.location.href= "sign_up.html"
+    window.location.href= "/sing/sign_up.html"
 })
 
